d5e75cffea8c687d6deffe0ee802e631a7c13b16
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2022-08-30 15:44:23 -0600

    router.post new video

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
-const videos = require('./routes/videos')
-const fs = require('fs')
+const videos = require('./routes/videos');
+const cors = require('cors');
+
+require('dotenv').config();
+const {PORT} = process.env;
+
+//CORS middleware
+app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.send("request received")
 })
 
 app.use('/videos', videos)
 
-app.listen('8080', () => {
+app.listen(PORT, () => {
     console.log("I hear you")
 })
